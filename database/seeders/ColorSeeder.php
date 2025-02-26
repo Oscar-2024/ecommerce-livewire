352eee268d3544723ac6ebdf482efa14d849282b
Oscar <DeveloperMAC2023@gmail.com>
2025-02-26 19:32:45 -0300

    Creación de Modelos - Migraciones - Seeder - Factorias

---
+++ database/seeders/ColorSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Color;
+use Illuminate\Database\Seeder;
+
+class ColorSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Color::factory()->create([
+            'name' => 'Black',
+        ]);
+
+        Color::factory()->create([
+            'name' => 'White',
+        ]);
+
+        Color::factory()->create([
+            'name' => 'Red',
+        ]);
+
+        Color::factory()->create([
+            'name' => 'Green',
+        ]);
+
+        Color::factory()->create([
+            'name' => 'Blue',
+        ]);
+
+        Color::factory()->create([
+            'name' => 'Purple',
+        ]);
+
+        Color::factory()->create([
+            'name' => 'Orange',
+        ]);
+    }
+}
